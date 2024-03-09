5f89a872765eeba42a8259be77fe803b7774d98e
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2024-03-09 11:13:39 +0200

    Remove debugging code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import todo from '/components/todo.js'
 
 const state = {}
 
-window.addEventListener("load", async _ => {
-  state.todo = await element(todo(state))
-  state.todo.appendTo(document.body)
-})
+state.todo = element(todo(state))
+state.todo.appendTo(document.body)
